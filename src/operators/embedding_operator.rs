e531191e7f495ef6b6c960b2e9391715d1168fb9
fastfist <fdenzell@gmail.com>
2023-08-28 14:03:48 -0500

    bugfix: fix request to embedding server

--- src/operators/embedding_operator.rs
+++ src/operators/embedding_operator.rs
@@ -1,11 +1,10 @@
-use ndarray::{Array, Array1};
 use serde::{Deserialize, Serialize};
 
 use crate::errors::ServiceError;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CustomServerData {
-    pub input: String,
+    pub input: Vec<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -13,14 +12,19 @@ pub struct CustomServerResponse {
     pub embeddings: Vec<f32>,
 }
 
-pub async fn create_embedding(messages: String) -> Result<Vec<f32>, ServiceError> {
+pub async fn create_embedding(message: String) -> Result<Vec<f32>, ServiceError> {
+    get_average_embedding(vec![message]).await
+}
+
+pub async fn get_average_embedding(document_chunks: Vec<String>) -> Result<Vec<f32>, ServiceError> {
+
     let embedding_server_call =
         std::env::var("EMBEDDING_SERVER_CALL").expect("EMBEDDING_SERVER_CALL must be set");
 
     let client = reqwest::Client::new();
     let resp = client
         .post(embedding_server_call)
-        .json(&CustomServerData { input: messages })
+        .json(&CustomServerData { input: document_chunks })
         .send()
         .await
         .map_err(ServiceError::EmbeddingServerCallError)?
@@ -29,17 +33,4 @@ pub async fn create_embedding(messages: String) -> Result<Vec<f32>, ServiceError
         .map_err(ServiceError::EmbeddingServerParseError)?;
 
     Ok(resp.embeddings)
-}
-pub async fn get_average_embedding(document_chunks: Vec<String>) -> Result<Vec<f32>, ServiceError> {
-    let mut embeddings: Vec<Vec<f32>> = vec![];
-    for chunk in document_chunks {
-        embeddings.push(create_embedding(chunk).await?);
-    }
-    let average_embedding: Array1<f32> = embeddings
-        .iter()
-        .map(|a| Array::from(a.clone()))
-        .reduce(|a, b| a + b)
-        .ok_or(ServiceError::EmbeddingAveragingError)?;
-
-    Ok(average_embedding.to_vec())
 }
