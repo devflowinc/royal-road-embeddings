a95e1da2b7407e923688b4ff76aa6baa9d1bb5fd
Nick K <nick@arguflow.gg>
2023-08-29 18:56:59 -0500

    feature: query to postgres to get qdrant_point_id's for the story_id[] and doc_group_size

---
+++ src/operators/doc_group_operator.rs
@@ -0,0 +1,28 @@
+use sqlx::{Pool, Postgres};
+
+use crate::{errors::ServiceError, operators::doc_embedding_operator::QdrantPointIdContainer};
+
+pub async fn get_doc_group_qdrant_ids_pg_query(
+    story_ids: Vec<i64>,
+    doc_group_size: i32,
+    pool: Pool<Postgres>,
+) -> Result<Vec<uuid::Uuid>, ServiceError> {
+    let qdrant_point_ids = sqlx::query_as!(
+        QdrantPointIdContainer,
+        r#"
+        SELECT qdrant_point_id
+        FROM doc_embeddings
+        WHERE story_id = ANY($1) AND doc_num = $2
+        "#,
+        story_ids.as_slice(),
+        doc_group_size as i64,
+    )
+    .fetch_all(&pool)
+    .await
+    .map_err(ServiceError::SelectDocGroupQdrantIdsPgError)?
+    .into_iter()
+    .map(|qdrant_point_id_container| qdrant_point_id_container.qdrant_point_id)
+    .collect::<Vec<uuid::Uuid>>();
+
+    Ok(qdrant_point_ids)
+}
--- src/operators/mod.rs
+++ src/operators/mod.rs
@@ -1,4 +1,5 @@
 pub mod doc_embedding_operator;
+pub mod doc_group_operator;
 pub mod embedding_operator;
 pub mod parse_operator;
 pub mod qdrant_operator;
