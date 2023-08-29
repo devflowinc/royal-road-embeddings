219e83c86611ce50e2434c7512e0eec5a01665b8
Nick K <nick@arguflow.gg>
2023-08-29 00:26:00 -0500

    feature: model to track DocGroupEmbedding

--- migrations/20230829051815_create-docgroupembedding-table.up.sql
+++ migrations/20230829051815_create-docgroupembedding-table.up.sql
@@ -1,16 +1,16 @@
 -- Add up migration script here
-CREATE TABLE doc_embeddings (
+CREATE TABLE doc_group_embeddings (
     id UUID NOT NULL UNIQUE PRIMARY KEY,
-    doc_html TEXT NOT NULL,
     story_id BIGINT NOT NULL,
-    doc_num BIGINT NOT NULL,
+    doc_group_size INTEGER NOT NULL,
+    index INTEGER NOT NULL,
     qdrant_point_id UUID NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT unique_story_id_doc_num_pair UNIQUE (story_id, doc_num)
+    CONSTRAINT unique_story_id_group_size_index UNIQUE (story_id, doc_group_size, index)
 );
  
 CREATE TRIGGER update_updated_at
-BEFORE UPDATE ON doc_embeddings
+BEFORE UPDATE ON doc_group_embeddings
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at();
