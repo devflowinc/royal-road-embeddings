68ecfdab03340cded97a381353183f1257ded90e
Nick K <nick@arguflow.gg>
2023-10-27 12:40:56 -0500

    cleanup: always perform a full re_index instead of attempting to only re-index relevant groups

--- src/handlers/embedding_handler.rs
+++ src/handlers/embedding_handler.rs
@@ -1,10 +1,13 @@
-use super::auth_handler::AuthRequired;
+use super::{auth_handler::AuthRequired, doc_group_handler::IndexDocumentGroupRequest};
 use crate::{
     data::models::DocEmbedding,
     errors::ServiceError,
     operators::{
-        doc_embedding_operator::{delete_doc_embedding_pg_query, upsert_doc_embedding_pg_query},
-        doc_group_embedding_operator::re_index_appropriate_doc_groups,
+        doc_embedding_operator::{
+            create_doc_group_embedding, delete_doc_embedding_pg_query,
+            upsert_doc_embedding_pg_query,
+        },
+        doc_group_embedding_operator::get_unique_doc_group_sizes,
         embedding_operator, parse_operator,
         qdrant_operator::delete_reinsert_doc_embedding_qdrant_query,
     },
@@ -32,7 +35,6 @@ pub async fn embed_document(
     _: AuthRequired,
 ) -> Result<HttpResponse, ServiceError> {
     let pool_inner = pool.get_ref().clone();
-    let pool_inner1 = pool.get_ref().clone();
 
     let doc_chunks = parse_operator::chunk_document(document.doc_html.clone())?;
 
@@ -56,8 +58,20 @@ pub async fn embed_document(
         upsert_doc_embedding_pg_query(doc_embedding_to_upsert.clone(), pool_inner.clone()).await?;
 
     if qdrant_point_id_to_delete.is_some() {
+        let unique_group_sizes =
+            get_unique_doc_group_sizes(vec![document.story_id], pool_inner.clone()).await?;
+
+        let pool_inner1 = pool.get_ref().clone();
+
         Arbiter::new().spawn(async move {
-            let _ = re_index_appropriate_doc_groups(document.story_id, document.index, pool_inner1);
+            for group_size in unique_group_sizes {
+                let doc_group_to_index = IndexDocumentGroupRequest::Story {
+                    story_id: document.story_id,
+                    doc_group_size: group_size,
+                };
+
+                let _ = create_doc_group_embedding(doc_group_to_index, pool_inner1.clone());
+            }
         });
     }
 
